98a68d79170860fa5c87f021d056e749643d1af8
Sean Anderson <sean@seananderson.ca>
2019-10-23 16:41:41 -0700

    Fix sdmTMB model to fit

--- R/sdmTMBPrac.R
+++ R/sdmTMBPrac.R
@@ -15,24 +15,29 @@ jchin <- readRDS(here::here("data", "juvCatchGSI_reg4.rds")) %>%
   #remove extra vars and stock ppn data
   select(-c(date, stableStation, samp_catch:SEAK))
 
-jchin_spde <- make_spde(jchin$xUTM_start, jchin$yUTM_start, n_knots = 100)
+range(jchin$xUTM_start)
+range(jchin$yUTM_start)
+range(jchin$ck_juv)
+hist(jchin$ck_juv)
+nrow(jchin)
+
+jchin$xUTM_start <- jchin$xUTM_start/10000
+jchin$yUTM_start <- jchin$yUTM_start/10000
+jchin_spde <- make_spde(jchin$xUTM_start, jchin$yUTM_start, n_knots = 150)
 plot_spde(jchin_spde)
 
-
 ## Develop index - FAILS TO CONVERGE 
-# Fit GLMM without spatiotemporal random fields or covariates
-m1 <- sdmTMB(ck_juv ~ 0 + as.factor(year), data = jchin,
-             time = "year", spde = jchin_spde,
-             family = tweedie(link = "log"))
+# Fit GLMM without covariates
+m1 <- sdmTMB(ck_juv ~ 0 + as.factor(year), 
+  data = jchin,
+  time = "year", 
+  spde = jchin_spde, 
+  silent = FALSE,
+  anisotropy = TRUE, 
+  include_spatial = TRUE,
+  ar1_fields = FALSE,
+  family = nbinom2(link = "log"))
+m1
 
-plot(ck_juv ~ jday, jchin)
 
-
-## Fit spatial trend models
-m1 <- sdmTMB(ck_juv ~ 1, data = jchin,
-             spde = jchin_spde, family = tweedie(link = "log"),
-             spatial_trend = TRUE, time = "year",
-             spatial_only = FALSE)
-
-hist(pcod$density)
-hist(jchin$ck_juv)
+# plot(ck_juv ~ jday, jchin)
