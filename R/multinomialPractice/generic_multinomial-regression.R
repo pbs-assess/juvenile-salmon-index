550e3395c3b0c3001e5cd416f0bbd7772001b97d
Cameron Freshwater <ccfreshwater@gmail.com>
2019-11-12 08:57:19 -0800

    nest function in function

--- R/multinomialPractice/generic_multinomial-regression.R
+++ R/multinomialPractice/generic_multinomial-regression.R
@@ -7,16 +7,11 @@ library(tidyverse)
 set.seed(42)
 N <- 100 # number of observations
 k <- 3 #number of groups
-
-### Gen function 1 - simulate data from variable number of groups 
-
 X <- runif(N) #predictor
 ints <- c(0.3, -1.4)
 betas <- c(-3, 4)
-k <- 3
 
-# Function guts
-cat <- seq(1, k, by = 1)
+# append with reference category
 int <- c(ints, NaN) 
 beta <- c(betas, NaN)
 
@@ -67,76 +62,78 @@ for (i in seq_along(y)) {
 ### Generic function 2 - estimate probabilities from input matrix (i.e. main
 # function)
 #Gen function 2 inputs 
-set.seed(42)
-k <- 3 #number of groups
-N <- 100
-
-# pars <- c(0.3, -1.4, -3, 4, 1, 1, 2, 2)
-cov <- matrix(data = runif(N), nrow = N, ncol = 1) #predictor
-
-nll <- function(pars) {
-  # ideally k and the covariate matrix should be arguments within the function,
-  # but not sure how to pass that to nlminb 
-  # k = 3
-  # cov = X
-  # pars <- c(0.3, -1.4, -3, 4)
-  
-  # define intercepts and betas based on pars vector and add  reference 
-  # category to vector of parameters
-  numBetas <- (length(pars) / (k-1)) - 1
-  int <- c(pars[1:(k-1)], NA)
-  betasVec <- pars[k:length(pars)]
-  beta <- matrix(NA, nrow = numBetas, ncol = k) 
-  for (j in 1:numBetas) {
-    beta[j , ] <- c(betasVec[(j * 2 - 1):(j * 2)], NA)
+multinomial <- function(Y, ints, betas, #pars, 
+                        cov, seed = NULL) {
+  if (is.numeric(seed)) {
+    set.seed(seed)    
   }
-  N <- nrow(cov)
+  N <- nrow(Y)
+  k <- ncol(Y)
+
+  # swap out ints and betas for pure vector if necessary
+  pars <- c(ints, betas)  
+  # pars <- c(0.3, -1.4, -3, 4, 1, 1, 2, 2)
+  # cov <- matrix(data = runif(N), nrow = N, ncol = 1) #predictor
   
-  ### adjusted to allow for multiple covariate effects, but not very elegant...
-  # calculate log-odds for each category 
-  log_odds <- matrix(NA, nrow = N, ncol = k)
-  for (h in 1:k) {
-    if (h < k) {
-      #calculate cumulative covariate effects
-      covEffects <- matrix(NA, nrow = N, ncol = numBetas)
-      for (j in 1:numBetas) {
-        covEffects[ , j] <- beta[j, h] * cov[ , j]
-      }
-      for (i in 1:N) {
-        log_odds[i , h] <- int[h] + sum(covEffects[i, ])
-      }
-    } else if (h == k) {
-      log_odds[ , h] <- NA
+  nll <- function(pars) {
+    # define intercepts and betas based on pars vector and add  reference 
+    # category to vector of parameters
+    numBetas <- (length(pars) / (k-1)) - 1
+    int <- c(pars[1:(k-1)], NA)
+    betasVec <- pars[k:length(pars)]
+    beta <- matrix(NA, nrow = numBetas, ncol = k) 
+    for (j in 1:numBetas) {
+      beta[j , ] <- c(betasVec[(j * 2 - 1):(j * 2)], NA)
     }
-  }
-  exp_log_odds <- exp(log_odds)
-  
-  denominator <- rep(NA, length.out = N)
-  for (i in 1:N) {
-    denominator[i] <- 1 + sum(exp_log_odds[i, 1:(k-1)])
-  }  
-  
-  probs <- matrix(NA, nrow = N, ncol = k)
-  for (h in 1:k) {
-    if (h < k) {
-      probs[ , h] <- exp_log_odds[ , h] / denominator
-    } 
-    else if (h == k) {
-      for (i in 1:N) {
-        #not sure if we can vectorize this without apply...
-        probs[i, h] <- 1 - sum(probs[i, 1:(k-1)]) 
+    N <- nrow(cov)
+    
+    ### adjusted to allow for multiple covariate effects, but not very elegant...
+    # calculate log-odds for each category 
+    log_odds <- matrix(NA, nrow = N, ncol = k)
+    for (h in 1:k) {
+      if (h < k) {
+        #calculate cumulative covariate effects
+        covEffects <- matrix(NA, nrow = N, ncol = numBetas)
+        for (j in 1:numBetas) {
+          covEffects[ , j] <- beta[j, h] * cov[ , j]
+        }
+        for (i in 1:N) {
+          log_odds[i , h] <- int[h] + sum(covEffects[i, ])
+        }
+      } else if (h == k) {
+        log_odds[ , h] <- NA
       }
     }
+    exp_log_odds <- exp(log_odds)
+    
+    denominator <- rep(NA, length.out = N)
+    for (i in 1:N) {
+      denominator[i] <- 1 + sum(exp_log_odds[i, 1:(k-1)])
+    }  
+    
+    probs <- matrix(NA, nrow = N, ncol = k)
+    for (h in 1:k) {
+      if (h < k) {
+        probs[ , h] <- exp_log_odds[ , h] / denominator
+      } 
+      else if (h == k) {
+        for (i in 1:N) {
+          #not sure if we can vectorize this without apply...
+          probs[i, h] <- 1 - sum(probs[i, 1:(k-1)]) 
+        }
+      }
+    }
+    
+    #log-likelihood
+    nll <- vector(length = length(y))
+    for (i in seq_along(y)) { # not vectorized
+      nll[i] <- -dmultinom(Y[i, ], size = 1, prob = probs[i, ], log = TRUE)
+    }
+    sum(nll)
   }
   
-  #log-likelihood
-  nll <- vector(length = length(y))
-  for (i in seq_along(y)) { # not vectorized
-    nll[i] <- -dmultinom(Y[i, ], size = 1, prob = probs[i, ], log = TRUE)
-  }
-  sum(nll)
-}
-# Fit with initial values
-m2 <- nlminb(rep(0, 4), nll)
-m2
-
+  # Fit with initial values
+  m2 <- nlminb(rep(0, 4), nll)
+  m2
+}  
+  
