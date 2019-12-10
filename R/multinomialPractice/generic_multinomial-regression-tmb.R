4c5b73d6314574f335f60220698a9cf24f1d7d49
Cameron Freshwater <ccfreshwater@gmail.com>
2019-12-10 15:23:11 -0800

    adjust to better accomodate fixed int. in TMB version of model

--- R/multinomialPractice/generic_multinomial-regression-tmb.R
+++ R/multinomialPractice/generic_multinomial-regression-tmb.R
@@ -3,20 +3,32 @@
 set.seed(1)
 N <- 2000 # number of observations
 k <- 4 #number of groups
-X <- runif(N) #predictor
-ints <- c(0.3, -1.4, 0.3)
-betas <- c(-3, 4, -1)
+# X <- runif(N) # cont. predictor
+#fixed predictor representing two cats
+yrCov <- data.frame(yr = sample(c("yr1", "yr2"), size = N, replace = T)) 
+X <- model.matrix(~yr, yrCov)
+
+int <- c(0.3, -1.4, 0.3)
+beta <- c(-0.25, 0.75, -0.5)
 
 # append with reference category
-int <- c(ints, NaN) 
-beta <- c(betas, NaN)
+#combine intercepts and betas into one matrix that can be multiplied by model
+#matrix; columns are parameters for each group (excluding ref. cat.)
+betas <- matrix(c(int, beta), nrow = ncol(X), ncol = k - 1, byrow = T)
+m <- nrow(betas) #number of covariates (i.e. 1 int. + slopes)
 
 log_odds <- matrix(NA, nrow = N, ncol = k)
-for (i in 1:k) {
-  if (i < k) {
-    log_odds[ , i] <- int[i] + beta[i] * X
-  } else if (i == k) {
-    log_odds[ , i] <- NA
+for (h in 1:k) {
+  if (h < k) {
+    cov_effects <- matrix(NA, nrow = N, ncol = m)
+    for (i in seq_len(N)) {
+      for (j in seq_len(m)) {
+        cov_effects[i, j] <- betas[j , h] * X[i, j]  
+      }
+      log_odds[i, h] <- sum(cov_effects[i, ])
+    }
+  } else if (h == k) {
+    log_odds[ , h] <- NA
   }
 }
 exp_log_odds <- exp(log_odds)
@@ -44,8 +56,7 @@ for (i in seq_along(y)) {
   temp <- rmultinom(1, 1, probs[i, ])
   y[i] <- which(temp == 1)
 }
-plot(X, jitter(y, 0.3))
-head(cbind(x = X, y))
+plot(X[ , 2], jitter(y, 0.3))
 
 #matrix of observations
 y_obs <- matrix(ncol = k, nrow = N, data = 0)
@@ -58,7 +69,8 @@ compile("R/multinomialPractice/multinomial_generic.cpp")
 dyn.load(dynlib("R/multinomialPractice/multinomial_generic"))
 
 ## Data and parameters
-.X <- cbind(1, X) #predictor with intercept
+# .X <- cbind(1, X) #predictor with intercept
+.X <-  model.matrix(~yr, yrCov) #intercept already bound
 data <- list(cov=.X, y_obs = y_obs)
 parameters <- list(betas = matrix(data = 0, nrow = ncol(.X), 
                                   ncol = ncol(y_obs) - 1))
