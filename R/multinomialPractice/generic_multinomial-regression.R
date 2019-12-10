9daa976c829049ed9ae7f3059c58037b9a072ef9
Cameron Freshwater <ccfreshwater@gmail.com>
2019-12-10 14:51:20 -0800

    adjust to better accomodate fixed int. in R version of model

--- R/multinomialPractice/generic_multinomial-regression.R
+++ R/multinomialPractice/generic_multinomial-regression.R
@@ -5,22 +5,32 @@ library(tidyverse)
 
 # structure inputs
 set.seed(42)
-N <- 100 # number of observations
+N <- 1000 # number of observations
 k <- 3 #number of groups
-X <- runif(N) #predictor
-ints <- c(0.3, -1.4)
-betas <- c(-3, 4)
+# X <- runif(N) #cont. predictor
+yrCov <- data.frame(yr = sample(c("yr1", "yr2"), size = N, replace = T)) #fixed predictor representing two cats
+.X <- model.matrix(~yr, yrCov)
 
-# append with reference category
-int <- c(ints, NaN) 
-beta <- c(betas, NaN)
+int <- c(0.3, -1.4)
+beta <- c(0.5, -0.5)
+
+#combine intercepts and betas into one matrix that can be multiplied by model
+#matrix; columns are parameters for each group (excluding ref. cat.)
+betas <- matrix(c(int, beta), nrow = ncol(.X), ncol = k - 1, byrow = T)
+m <- nrow(betas) #number of covariates (i.e. 1 int. + slopes)
 
 log_odds <- matrix(NA, nrow = N, ncol = k)
-for (i in 1:k) {
-  if (i < k) {
-    log_odds[ , i] <- int[i] + beta[i] * X
-  } else if (i == k) {
-    log_odds[ , i] <- NA
+for (h in 1:k) {
+  if (h < k) {
+    cov_effects <- matrix(NA, nrow = N, ncol = m)
+    for (i in seq_len(N)) {
+      for (j in seq_len(m)) {
+        cov_effects[i, j] <- betas[j , h] * .X[i, j]  
+      }
+      log_odds[i, h] <- sum(cov_effects[i, ])
+    }
+  } else if (h == k) {
+    log_odds[ , h] <- NA
   }
 }
 exp_log_odds <- exp(log_odds)
@@ -29,7 +39,7 @@ denominator <- rep(NA, length.out = N)
 for (i in 1:N) {
   denominator[i] <- 1 + sum(exp_log_odds[i, 1:(k-1)])
 }  
-  
+
 probs <- matrix(NA, nrow = N, ncol = k)
 for (i in 1:k) {
   if (i < k) {
@@ -37,7 +47,6 @@ for (i in 1:k) {
   } 
   else if (i == k) {
     for (h in 1:N) {
-      #not sure if we can vectorize this without apply...
       probs[h, i] <- 1 - sum(probs[h, 1:(k-1)]) 
     }
   }
@@ -49,8 +58,8 @@ for (i in seq_along(y)) {
   temp <- rmultinom(1, 1, probs[i, ])
   y[i] <- which(temp == 1)
 }
-plot(X, jitter(y, 0.3))
-head(cbind(x = X, y))
+
+plot(.X[ , 2], jitter(y, 0.3))
 
 #matrix of observations
 Yobs <- matrix(ncol = k, nrow = N, data = 0)
@@ -58,46 +67,33 @@ for (i in seq_along(y)) {
   Yobs[i, y[i]] <- 1
 }
 
-saveRDS(Yobs, here::here("R", "multinomialPractice", "exDat.RDS"))
+# saveRDS(Yobs, here::here("R", "multinomialPractice", "exDat.RDS"))
 Yobs <- readRDS(here::here("R", "multinomialPractice", "exDat.RDS"))
 
-### Generic function 2 - estimate probabilities from input matrix (i.e. main
-# function)
-#Gen function 2 inputs 
-# swap out ints and betas for pure vector if necessary
-pars <- c(ints, betas)  
-pars <- c(0.3, -1.4, -3, 4, 1, 1, 2, 2)
 
 set.seed(42)
-N <- nrow(Yobs) # number of observations
-k <- ncol(Yobs) # number of groups
-covMatrix <- matrix(data = runif(N), nrow = N, ncol = 1) #predictor
+yrCov <- data.frame(yr = sample(c("yr1", "yr2"), size = nrow(Yobs), 
+                                replace = T)) #fixed predictor representing two cats
+.X <- model.matrix(~yr, yrCov)
 
-
-nll <- function(pars, Y, k, covMatrix) {
+nll <- function(pars, Y, covMatrix) {
+  k <- ncol(Y)
   N <- nrow(Y) # number of rows in observation matrix
   
-  # define intercepts and betas based on pars vector and add  reference 
+  # define intercepts and betas based on pars vector and add reference 
   # category to vector of parameters
-  numBetas <- ncol(covMatrix)
-  int <- c(pars[1:(k-1)], NA)
-  betasVec <- pars[k:length(pars)]
-  beta <- matrix(NA, nrow = numBetas, ncol = k) 
-  for (j in 1:numBetas) {
-    beta[j , ] <- c(betasVec[(j * 2 - 1):(j * 2)], NA)
-  }
-  ### adjusted to allow for multiple covariate effects, but not very elegant...
-  # calculate log-odds for each category 
+  betas <- matrix(pars, nrow = ncol(covMatrix), ncol = k - 1, byrow = T)
+  m <- nrow(betas) #number of covariates (i.e. 1 int. + slopes)
+  
   log_odds <- matrix(NA, nrow = N, ncol = k)
   for (h in 1:k) {
     if (h < k) {
-      #calculate cumulative covariate effects
-      covEffects <- matrix(NA, nrow = N, ncol = numBetas)
-      for (j in 1:numBetas) {
-        covEffects[ , j] <- beta[j, h] * covMatrix[ , j]
-      }
-      for (i in 1:N) {
-        log_odds[i , h] <- int[h] + sum(covEffects[i, ])
+      cov_effects <- matrix(NA, nrow = N, ncol = m)
+      for (i in seq_len(N)) {
+        for (j in seq_len(m)) {
+          cov_effects[i, j] <- betas[j , h] * covMatrix[i, j]  
+        }
+        log_odds[i, h] <- sum(cov_effects[i, ])
       }
     } else if (h == k) {
       log_odds[ , h] <- NA
@@ -132,9 +128,12 @@ nll <- function(pars, Y, k, covMatrix) {
 }
 
 set.seed(42)
-X <- matrix(data = runif(N), nrow = N, ncol = 1) #predictor
+# .X <- matrix(data = runif(N), nrow = N, ncol = 1) #predictor
+yrCov <- data.frame(yr = sample(c("yr1", "yr2"), size = nrow(Yobs), 
+                                     replace = T)) #fixed predictor representing two cats
+.X <- model.matrix(~yr, yrCov)
 
 # Fit with initial values
-m2 <- nlminb(rep(0, 4), nll, Y = Yobs, k = 3, covMatrix = X)
+m2 <- nlminb(rep(0, 4), nll, Y = Yobs, covMatrix = .X)
 m2
 
