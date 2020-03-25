1e5a6f99a1665a4ba4aa0f62e0749bb5c1f77439
CamFreshwater <ccfreshwater@gmail.com>
2020-03-25 09:07:22 -0700

    sim confirms unbiased estimates

--- R/sim_practice/multinomialPractice/sim_combined.R
+++ R/sim_practice/multinomialPractice/sim_combined.R
@@ -15,7 +15,7 @@ N_m1 <- n_sites * n_m1_site
 fix_ints_m1 <- c(0.75, 1) 
 f_dat_m1 <- data.frame(reg = as.factor(sample(c(1, 2), size = N_m1, replace = T)))
 fix_mm1 <- model.matrix(~ reg, data = f_dat_m1)
-
+log_phi <- log(1.1)
 
 ## Parameters for multinomial data
 n_m2_site <- 250
@@ -53,7 +53,7 @@ f_sim <- function(trial = 1) {
     mutate(fe = as.vector(fe_m1),
            mu = mu_fe_m1,
            site_obs = tweedie::rtweedie(N_m1, mu = mu_fe_m1, 
-                                        power = 2, phi = 1))
+                                        power = 1.5, phi = exp(log_phi)))
   
   
   ## Multinomial composition data
@@ -101,7 +101,14 @@ f_sim <- function(trial = 1) {
   Y_m2 <- matrix(ncol = 3, nrow = N_m2, data = 0)
   for (i in seq_along(y)) Y_m2[i, y[i]] <- 1
   
-  return(list(dat_m1 = dat_m1, dat_m2 = dat_m2, obs_m2 = Y_m2))
+  # vector of true values to compare during simulation testing
+  true_values <- c(fix_ints_m1, log_phi, 1.5, log(sd_m1_site), 
+                  as.vector(fix_ints_m2), log(sd_m2_site), site_mean_m1,
+                  site_mean_m2)
+
+  
+  return(list(dat_m1 = dat_m1, dat_m2 = dat_m2, obs_m2 = Y_m2, 
+              true_val = true_values))
 }
 
 sim_dat <- f_sim()
@@ -193,9 +200,6 @@ sdr
 ssdr <- summary(sdr)
 ssdr
 
-saveRDS(ssdr, here::here("data", "modelFits", "temp_combo_ssdr.rds"))
-ssdr <- readRDS(here::here("data", "modelFits", "temp_combo_ssdr.rds"))
-
 
 # PREDICTIONS ------------------------------------------------------------------
 log_pred <- ssdr[rownames(ssdr) %in% "log_pred_abund", ]
@@ -239,7 +243,6 @@ ggplot() +
   facet_wrap(~ reg, nrow = 2, scales = "free_y") +
   ggsidekick::theme_sleek()
 
-
 # proportions are right 
 ggplot() +
   geom_point(data = raw_abund %>% select(cat, reg, ppn) %>% distinct(), 
@@ -271,3 +274,109 @@ ggplot() +
                                       ymax = raw_abund_up), color= "red") +
   facet_wrap(~ reg, nrow = 2, scales = "free_y") +
   ggsidekick::theme_sleek()
+
+
+# SIMULATION -------------------------------------------------------------------
+
+# Simulate combined model to ensure parameter estimates can be succesfully 
+# recovered
+dat_list <- vector(mode = "list", length = 50)
+for (i in seq_along(dat_list)) {
+  dat_list[[i]] <- f_sim(trial = i)
+}
+
+
+sim_ests <- map(dat_list, function(x) {
+  m1_dat <- x$dat_m1
+  m2_dat <- x$dat_m2
+  m2_obs <- x$obs_m2
+  
+  mm_pred <- fix_mm1 %>% 
+    unique() 
+  ord_mat <- mm_pred %>% 
+    apply(., 1, sum) %>%
+    sort() %>% 
+    names()
+  mm_pred <- mm_pred[ord_mat, ]
+  
+  # fit dummy model to speed up tweedie abundance estimates
+  m1 <- lm(log(site_obs) ~ reg, data = m1_dat %>% filter(site_obs > 0))
+  
+  fac1k <- fct_to_tmb_num(m1_dat$site)
+  fac2k <- fct_to_tmb_num(m2_dat$site)
+  
+  # generate factor key for multinomial data
+  fac_dat <- m2_dat %>% 
+    mutate(facs = reg,#as.factor(paste(reg, sep = "_")),
+           facs_n = fct_to_tmb_num(facs)) %>% #subtract for indexing by 0 
+    select(reg, facs, facs_n)
+  fac_key <- fac_dat %>% 
+    distinct() %>% 
+    arrange(facs_n)
+  
+  # combine
+  data <- list(
+    #abundance data
+    y1_i = m1_dat$site_obs,
+    X1_ij = fix_mm1,
+    factor1k_i = fac1k,
+    nk1 = length(unique(fac1k)),
+    X1_pred_ij = mm_pred,
+    #composition data
+    y2_ig = m2_obs,
+    X2_ij = fix_mm2,
+    factor2k_i = fac2k,
+    nk2 = length(unique(fac2k)),
+    m2_all_fac = fac_dat$facs_n,
+    m2_fac_key = fac_key$facs_n
+  )
+  
+  parameters = list(
+    b1_j = coef(m1) + rnorm(length(coef(m1)), 0, 0.01),#rep(1, ncol(fix_mm)),
+    log_phi = log(1.1),
+    logit_p = boot::logit(0.8),
+    z1_k = rep(0, length(unique(fac1k))),
+    log_sigma_zk1 = log(0.25),
+    b2_jg = matrix(0, nrow = ncol(fix_mm2), ncol = ncol(m2_obs) - 1),
+    z2_k = rep(0, times = length(unique(fac2k))),
+    log_sigma_zk2 = log(0.25)
+  )
+  
+  ## Make a function object
+  compile("R/sim_practice/tweediePractice/tweedie_multinomial_1re.cpp")
+  dyn.load(dynlib("R/sim_practice/tweediePractice/tweedie_multinomial_1re"))
+  obj <- MakeADFun(data, parameters, random = c("z1_k", "z2_k"), 
+                   DLL = "tweedie_multinomial_1re")
+  
+  opt <- nlminb(obj$par, obj$fn, obj$gr)
+  sdr <- sdreport(obj)
+  ssdr <- summary(sdr)
+  
+  # export data
+  trim_ssdr <- ssdr[!rownames(ssdr) %in% 
+                      c("log_pred_abund", "logit_pred_prob", "pred_abund_mg"), ]
+  as.data.frame(trim_ssdr) %>% 
+    mutate(par = c("b1_j1", "b1_j2", "log_phi", "logit_p", "log_sigma_zk1",
+                   paste("b2_jg", seq(from = 1, to = 4, by = 1), sep = ""),
+                   "log_sigma_zk2",
+                   paste("z1_k", seq(from = 1, to = 5, by = 1), sep = ""),
+                   paste("z2_k", seq(from = 1, to = 5, by = 1), sep = "")),
+           trial = unique(x$dat_m1$trial),
+           true_val = x$true_val)
+}) %>% 
+  bind_rows() 
+
+
+p_dat <- sim_ests %>% 
+  filter(par == "logit_p") %>% 
+  mutate(Estimate = 1.0 / (1.0 + exp(-Estimate)) + 1.0)
+  
+sim_ests %>% 
+  filter(par %in% c("b1_j1", "b1_j2", "log_phi", "log_sigma_zk1",
+                    paste("b2_jg", seq(from = 1, to = 4, by = 1), sep = ""),
+                    "log_sigma_zk2")) %>%
+  rbind(p_dat) %>% 
+  ggplot(.) +
+  geom_boxplot(aes(x = par, y = Estimate)) +
+  geom_point(aes(x = par, y = true_val), colour = "red")
+
