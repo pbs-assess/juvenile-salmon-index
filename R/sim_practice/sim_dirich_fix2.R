a705d88f92f41595ea85f42f467a31e5692e69e3
CamFreshwater <ccfreshwater@gmail.com>
2020-06-02 12:12:19 -0700

    fix bug in new FE dirichlet model; compare to package

--- R/sim_practice/sim_dirich_fix2.R
+++ R/sim_practice/sim_dirich_fix2.R
@@ -14,6 +14,7 @@ n_sites <- 5
 n_obs_per_site <- 100
 sd_site <- 0.5
 n <- n_sites * n_obs_per_site
+n_groups <- 3
 
 #covdat
 reg_dat <- data.frame(reg = as.factor(sample(c(1, 2, 3), size = n, 
@@ -24,17 +25,18 @@ reg_dat <- data.frame(reg = as.factor(sample(c(1, 2, 3), size = n,
 fix_mm <- model.matrix(~ (reg + fac), reg_dat)
 
 # fixed intercepts (P x K)
-P <- ncol(fix_mm) - 1
-K <- 4
-betas <- matrix(rnorm((P+1)*K), (P+1), K)
+P <- ncol(fix_mm)
+K <- n_groups - 1 #groups - 1 for reference category
+betas <- matrix(rnorm(P * K), P, K)
+log_phi <- 3
 
 # function to simulate dirichlet data based on parameters and matrix
-f_sim <- function(trial = 1, scalar = 100) {
+f_sim <- function(trial = 1) {
   
   #calculate fixed effects using parameter matrix and model matrix
   n_fix_cov <- P
   fix_eff <- fix_mm %*% betas
-  sum_fix_eff <- apply(fix_eff, 1, sum)
+  colnames(fix_eff) <- paste("beta_k", seq(2, K+1, by = 1), sep = "")
   
   # generate random effects, then combine with fixed
   site_mean_a <- rnorm(mean = 0,
@@ -49,28 +51,21 @@ f_sim <- function(trial = 1, scalar = 100) {
                          site_obs = site_obs_ia,
                          sd_site = sd_site
   ) %>%
-    cbind(., reg_dat, sum_fix_eff)  #bind regional fixed effects data
+    cbind(., reg_dat, fix_eff)  #bind regional fixed effects data
   
-  #calc gamma
-  Gamma = exp(fix_eff) #fixed effects
-  Gamma_plus = apply(Gamma, 1, sum) #sum of fixed_effects
-  theta = 1 / (Gamma_plus + 1)
-  pi = apply(Gamma, 2, function(x) {x / theta})
-  
-  #number of values to simulate per row of input
-  N <- sample(c(1:1000), n, replace = T)
-  
+  #calc effects
+  mu_eff <- cbind(1, exp(fix_eff))
+  ma <- mu_eff / rowSums(mu_eff)
+  ba <- exp(log_phi) * ma
+
   # Simulate the responses Y from package 'dirmult'
-  Y = simPop(J = 1, n = N[1], pi = pi[1,], theta = theta[1])$data
-  for(jj in c(2:n)){
-    Y = rbind(Y, simPop(J = 1, n = N[jj], pi = pi[jj,], theta = theta[jj])$data)
+  Y = rdirichlet(n = 1, ba[1, ])
+  for(j in c(2:n)){
+    Y = rbind(Y, rdirichlet(n = 1, ba[j, ]))
   }
   
-  #switch to proportions and add small value
-  Y_adj <- Y / rowSums(Y)  
-  Y_adj <- Compositional::zeroreplace(Y_adj, a = 2/3)  
-  #scalar is necessary to produce realistic SEs
-  Y_adj <- Y_adj * scalar
+  #add small value to any zeros
+  Y_adj <- zeroreplace(Y, a = 2/3)  
   
   # create the observations:
   datf <- cbind(site_dat, Y_adj)
@@ -82,19 +77,19 @@ f_sim <- function(trial = 1, scalar = 100) {
 
 n_trials <- 5
 sims <- vector(mode = "list", length = n_trials)
-sims_100 <- vector(mode = "list", length = n_trials)
+# sims_100 <- vector(mode = "list", length = n_trials)
 for (i in 1:n_trials) {
-  sims_dat <- f_sim(trial = i, scalar = 100)
+  sims_dat <- f_sim(trial = i)
   sims[[i]] <- list(trial = i, obs = sims_dat$obs,
                     full_dat = sims_dat$full_data, trans = "raw")
-  sims_100[[i]] <- list(trial = i, obs = sims_dat$obs_adj,
-                        full_dat = sims_dat$full_data, trans = "adj100")
+  # sims_100[[i]] <- list(trial = i, obs = sims_dat$obs_adj,
+  #                       full_dat = sims_dat$full_data, trans = "adj100")
 }
 
-sim_list <- c(sims, sims_100)
+# sim_list <- c(sims, sims_100)
 
 #initial parameter values
-beta_in <- matrix(rnorm(n = (P+1)*K, mean = 0), (P+1), K)
+beta_in <- matrix(rnorm(n = P * K, mean = 0), P, K)
 
 #prediction model matrix
 pred_dat <- reg_dat %>%
@@ -103,15 +98,30 @@ pred_dat <- reg_dat %>%
   arrange(reg, fac)
 pred_mm <- model.matrix(~ reg + fac, pred_dat)
 
+# fit compositional package model
+temp_x <- sims[[1]]$full_dat %>% 
+  select(reg, fac)
+mod1 <- Compositional::diri.reg(y = sims[[1]]$obs, x = temp_x,
+                                xnew = pred_dat)
+
 compile(here::here("src", "dirichlet_fixInt_v2.cpp"))
 dyn.load(dynlib(here::here("src", "dirichlet_fixInt_v2")))
 
-
-Y_in <- sims_in$obs
-
 obj <- MakeADFun(data=list(fx_cov = fix_mm,
-                           y_obs = Y_in,
+                           y_obs = sims[[1]]$obs,
                            pred_cov = pred_mm),
-                 parameters=list(z_ints = beta_in),
-                 DLL="dirichlet_fixInt")
+                 parameters=list(z_ints = beta_in,
+                                 log_phi = runif(1, 1, 10)),
+                 DLL="dirichlet_fixInt_v2")
 opt <- nlminb(obj$par, obj$fn, obj$gr)
+sdr <- sdreport(obj)
+ssdr <- summary(sdr)
+
+# check estimates
+matrix(ssdr[rownames(ssdr) %in% "z_ints", "Estimate"], ncol = K)
+mod1$be
+betas
+ssdr[rownames(ssdr) %in% "log_phi", "Estimate"]
+mod1$log.phi
+matrix(ssdr[rownames(ssdr) %in% "pred_est", "Estimate"], ncol = n_groups)
+mod1$est
--- src/dirichlet_fixInt_v2.cpp
+++ src/dirichlet_fixInt_v2.cpp
@@ -14,10 +14,8 @@ Type objective_function<Type>::operator() ()
 
   // The dimensions of data
   int n_obs = y_obs.rows();         // number of observations
-  // int n_cat = y_obs.cols();         // number of categories
-  int n_fix_cov = fx_cov.cols();    // number of types of fixed covariates in m
-  // int n_levels = pred_cov.rows();   // number of covariates to make predictions on
-
+  int n_groups = y_obs.cols();      // number of categories
+  int n_preds = pred_cov.rows();         // number of predictions
   
   // calculate fixed effects
   matrix<Type> log_y = log(y_obs.array());
@@ -25,24 +23,24 @@ Type objective_function<Type>::operator() ()
   matrix<Type> exp_fx_eff = exp(fx_eff.array());
 
   // add column of 1s (intercepts) to exp_fix_eff matrix
-  matrix<Type> mu1(n_obs, (n_fix_cov + 1));
+  matrix<Type> mu1(n_obs, n_groups);
   for(int i = 0; i < n_obs; i++) {
     mu1(i, 0) = 1;
-    for(int m = 0; m < n_fix_cov; m++){
-      mu1(i, (m + 1)) = exp_fx_eff(i, m);
+    for(int k = 0; k < (n_groups - 1); k++) {
+      mu1(i, (k + 1)) = exp_fx_eff(i, k);
     }
   }
   vector<Type> mu1_sum = mu1.rowwise().sum();
   
   // calc rowwise sums
-  matrix<Type> ma(n_obs, (n_fix_cov + 1));
-  matrix<Type> ba(n_obs, (n_fix_cov + 1));
+  matrix<Type> ba(n_obs, n_groups);
+  matrix<Type> ma(n_obs, n_groups);
   Type phi = exp(log_phi);
 
   for (int i = 0; i < n_obs; i++) {
-    for (int m = 0; m < (n_fix_cov + 1); m++) {
-      ma(i, m) = mu1(i, m) / mu1_sum(i);
-      ba(i, m) = phi * ma(i, m);
+    for (int k = 0; k < n_groups; k++) {
+      ma(i, k) = mu1(i, k) / mu1_sum(i);
+      ba(i, k) = phi * ma(i, k);
     }
   }  
 
@@ -50,36 +48,38 @@ Type objective_function<Type>::operator() ()
   Type jnll = 0;
   jnll = -1 * n_obs * lgamma(phi);
   for (int i = 0; i < n_obs; i++) {
-    for (int m = 0; m < (n_fix_cov + 1); m++) {
-      jnll += lgamma(ba(i, m));
-      jnll -= log_y(i, m) * (ba(i, m) - 1);
+    for (int k = 0; k < n_groups; k++) {
+      jnll += lgamma(ba(i, k));
+      jnll -= log_y(i, k) * (ba(i, k) - 1);
     }
   }
 
   // calculate predictions
+  matrix<Type> pred_mu1(n_preds, n_groups);
+  matrix<Type> pred_est(n_preds, n_groups);
+
   matrix<Type> pred_eff = pred_cov * z_ints;
   matrix<Type> exp_pred_eff = exp(pred_eff.array());
-
+  
   // add column of 1s (intercepts) to prediction model matrix
-  matrix<Type> pred_mu1(n_obs, (n_fix_cov + 1));
-  for(int i = 0; i < n_obs; i++) {
-    pred_mu1(i, 0) = 1;
-    for(int m = 0; m < n_fix_cov; m++){
-      pred_mu1(i, (m + 1)) = exp_pred_eff(i, m);
+  for(int j = 0; j < n_preds; j++) {
+    pred_mu1(j, 0) = 1;
+    for(int k = 0; k < (n_groups - 1); k++) {
+      pred_mu1(j, (k + 1)) = exp_pred_eff(j, k);
     }
   }
   vector<Type> pred_mu1_sum = pred_mu1.rowwise().sum();
   
-  for (int i = 0; i < n_obs; i++) {
-    for (int m = 0; m < (n_fix_cov + 1); m++) {
-      pred_est(i, m) = pred_mu1(i, m) / pred_mu1_sum(i);
+  for (int j = 0; j < n_preds; j++) {
+    for (int k = 0; k < (n_groups); k++) {
+      pred_est(j, k) = pred_mu1(j, k) / pred_mu1_sum(j);
     }
   } 
 
-  REPORT(pred_gamma);
-  ADREPORT(pred_gamma);
+  REPORT(pred_est);
+  ADREPORT(pred_est);
   
-  Return negative loglikelihood
+  // Return negative loglikelihood
   return jnll;
   
 }
